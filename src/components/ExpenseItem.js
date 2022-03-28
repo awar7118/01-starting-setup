38408bc4ee653ffe2eb9c24ae96b6b4fe0daa1e3
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-28 17:32:47 +0100

    added classNames

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className="expense-item">
+      <div>March 28th 2022</div>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price ">294.97</div>
       </div>
     </div>
   );
