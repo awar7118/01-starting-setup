d42cbc175c93e71afc4bc17599a0b9018056c8cb
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-28 17:34:46 +0100

    new date constant created

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
   return (
     <div className="expense-item">
       <div>March 28th 2022</div>
