5bd186e99a601dafab8365899ea57034b4acfca7
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-29 08:15:07 +0100

    showing dates

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,2 +1,13 @@
-function ExpenseDate() {}
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-UK", { month: "short" });
+  const year = props.date.toLocaleString("en-UK", { year: "2-digit" });
+  const day = props.date.getFullYear();
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+}
 export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,14 +2,9 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-UK", { month: "short" });
-  const year = props.date.toLocaleString("en-UK", { year: "2-digit" });
-  const day = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price ">{props.amount}</div>
