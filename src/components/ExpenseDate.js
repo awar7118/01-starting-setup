6f0d33df43f1209c1dccec2202eaef0a3481c3ee
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-29 08:20:18 +0100

    expensedate components layout improved

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,13 @@
+import "./ExpenseDate.css";
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-UK", { month: "short" });
   const year = props.date.toLocaleString("en-UK", { year: "2-digit" });
   const day = props.date.getFullYear();
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year ">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
