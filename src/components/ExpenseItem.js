442a65310cdb8787f77ff420bd58f83e964f069c
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-29 07:55:40 +0100

    created expensedate component and updating the dates for props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
+  const month = props.date.toLocaleString("en-UK", { month: "short" });
   return (
     <div className="expense-item">
-      <div>Month</div>
-      <div>Year</div>
-      <div>Date</div>
+      <div>{month}</div>
+      <div>year</div>
+      <div>day</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price ">{props.amount}</div>
