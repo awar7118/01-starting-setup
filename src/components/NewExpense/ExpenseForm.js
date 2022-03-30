637d16019573541ded2b95f7dbd0b4131bcd5f52
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-30 10:01:34 +0100

    addedd listeners to both the amount and date and stored it

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,18 @@ const ExpenseForm = () => {
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
+  const [enteredAmount, setEnteredAmount] = useState(" ");
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // console.log(event.target.value);
+  };
+  const [enteredDate, setEnteredDate] = useState(" ");
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // console.log(event.target.value);
+    // if 30/03/2022 selected it shows 2022-03-30 when console logged.
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
@@ -14,11 +26,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-expense__control">
           <label>date</label>
-          <input type="date" min="2022-01-02" max="2022-12-21" />
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            min="2022-01-02"
+            max="2022-12-21"
+          />
         </div>
       </div>
       <div className="new-expense__actions">
