eeef08997a72fe2bc430470bd70941b5de520b24
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-30 09:45:56 +0100

    expenseform updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -23,7 +24,12 @@ function App() {
   ];
   return (
     <div>
-      <Expenses items={expenses} />
+      <div>
+        <NewExpense />
+      </div>
+      <div>
+        <Expenses items={expenses} />
+      </div>
     </div>
   );
 }
