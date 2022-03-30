ccae15be4dfacfccd726daa1aa97d729e131b02d
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-30 12:01:12 +0100

    fixed bug with form input no being parsed/out of range

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,17 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
   // array destructuring is how line 5 works.
-  const [enteredTitle, setEnteredTitle] = useState(" ");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
-  const [enteredAmount, setEnteredAmount] = useState(" ");
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-    // console.log(event.target.value);
   };
-  const [enteredDate, setEnteredDate] = useState(" ");
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-    // console.log(event.target.value);
-    // if 30/03/2022 selected it s hows 2022-03-30 when console logged.
   };
 
   const submitHandler = (event) => {
@@ -26,41 +23,50 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
+
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
-            onChange={amountChangeHandler}
             min="0.01"
             step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense__control">
-          <label>date</label>
+          <label>Date</label>
           <input
             type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
-            min="2022-01-02"
-            max="2022-12-21"
           />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit" onSubmit={submitHandler}>
-          Add Expense
-        </button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
 };
+
 export default ExpenseForm;
