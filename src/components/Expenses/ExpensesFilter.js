9a0192c3c4a66681aa8ec6801aa3e93725fe5d5e
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-04-06 21:18:10 +0100

    added dropdownchangehandler and pointed at it onchange of dropwdown:

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -4,26 +4,18 @@ import "./ExpensesFilter.css";
 
 const ExpensesFilter = () => {
   // const [selectedDate, setselectedDate] = useState(" ");
-  const dateSelector = (event) => {
-    console.log("hello");
+  const dropdownChangeHandler = (event) => {
+    console.log(event.target.value);
   };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value="2022" onClick={dateSelector}>
-            2022
-          </option>
-          <option value="2021" onClick={dateSelector}>
-            2021
-          </option>
-          <option value="2020" onClick={dateSelector}>
-            2020
-          </option>
-          <option value="2019" onClick={dateSelector}>
-            2019
-          </option>
+        <select onChange={dropdownChangeHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
