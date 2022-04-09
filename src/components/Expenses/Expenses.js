a8680a729ecde8f2a9a4b56e9687cee99e85822a
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-04-09 20:17:18 +0100

    expenseslist component and contents included

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import "./ExpensesFilter";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -14,22 +14,7 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expenseDisplayContent = (
-    <p style={{ textAlign: "center", color: "white" }}>
-      Feel free to input your first expense! 🚀{" "}
-    </p>
-  );
-  if (filteredExpenses.length > 0) {
-    expenseDisplayContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        // if you add key, it will help react uniquely identify each item. Always add key when mapping list of items
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      ></ExpenseItem>
-    ));
-  }
+
   return (
     <div>
       <Card className="expenses">
@@ -39,7 +24,7 @@ function Expenses(props) {
             onChangeFilter={filterChangeHandler}
           />
         </div>
-        {expenseDisplayContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <h2 className="expenses-list__fallback">
+        Feel free to input your first expense! 🚀
+      </h2>
+    );
+  }
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          // if you add key, it will help react uniquely identify each item. Always add key when mapping list of items
+          key={expense.id}
+          s
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        ></ExpenseItem>
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
